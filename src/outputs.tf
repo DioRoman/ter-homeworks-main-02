fcd27b95c3c2701a0b6ed524a1607b6a58d34747
Roman L <roman-ronin@yandex.ru>
2025-06-08 16:04:07 +0300

    9.1

--- src/outputs.tf
+++ src/outputs.tf
@@ -2,8 +2,8 @@ output "VM-stats" {
 
   value = [
 
-    { vm1 = [yandex_compute_instance.web.name, yandex_compute_instance.web.network_interface[0].nat_ip_address, yandex_compute_instance.web.fqdn ] },
-    { vm2 = [yandex_compute_instance.db.name, 
+    { web = [yandex_compute_instance.web.name, yandex_compute_instance.web.network_interface[0].nat_ip_address, yandex_compute_instance.web.fqdn ] },
+    { db = [yandex_compute_instance.db.name, 
     yandex_compute_instance.db.network_interface[0].nat_ip_address, yandex_compute_instance.db.fqdn ] }
 
   ]
